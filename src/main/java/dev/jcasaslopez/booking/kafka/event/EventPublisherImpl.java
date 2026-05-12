11cbc1a6ff9b69109d20f88bc5f37e75d9d38a46
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-05-12 13:36:28 +0200

    feat: add event publisher implementation

--- src/main/java/dev/jcasaslopez/booking/kafka/event/EventPublisherImpl.java
+++ src/main/java/dev/jcasaslopez/booking/kafka/event/EventPublisherImpl.java
@@ -18,7 +18,7 @@ import dev.jcasaslopez.classroom.shared.event.ClassroomEvent;
 import dev.jcasaslopez.classroom.shared.event.NotificationEvent;
 
 public class EventPublisherImpl implements EventPublisher {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(EventPublisherImpl.class);
 	private List<ClassroomEvent> classroomsStore;
 	private NotificationEventProducer notificationEventProducer;
@@ -35,41 +35,48 @@ public class EventPublisherImpl implements EventPublisher {
 	public void publishBookingRelatedEvent(NotificationType type, Booking booking, String email) {
 		if (!Set.of(NotificationType.BOOKING_CONFIRMED, NotificationType.BOOKING_CANCELLED, 
 				NotificationType.WATCH_ALERT_TRIGGERED).contains(type)) {	
-		    logger.warn("Invalid notification type {} for booking event", type);
+			logger.warn("Invalid notification type {} for booking event", type);
 			throw new IllegalArgumentException("Invalid notification type for booking event: " + type);
 		}
-		notificationEventProducer.sendNotification(createNotificationEvent(type, booking, email));
+		NotificationEvent notification = createNotificationEvent(type, booking, email);
+		notificationEventProducer.sendNotification(notification);
 	}
 
 	@Override
 	public void publishBookingRelatedEvent(NotificationType type, WatchAlert watchAlert, String email) {
 		if (!Set.of(NotificationType.WATCH_ALERT_CONFIRMED).contains(type)) {
-		    logger.warn("Invalid notification type {} for booking event", type);
+			logger.warn("Invalid notification type {} for booking event", type);
 			throw new IllegalArgumentException("Invalid notification type for booking event: " + type);
 		}
 		Booking booking = bookingRepository.findById(watchAlert.getIdBooking())
-			    .orElseThrow(() -> new NoSuchBookingException("Booking was not found in the database"));		
-		notificationEventProducer.sendNotification(createNotificationEvent(type, booking, email));
+				.orElseThrow(() -> new NoSuchBookingException("Booking was not found in the database"));		
+		NotificationEvent notification = createNotificationEvent(type, booking, email);
+		notificationEventProducer.sendNotification(notification);	
 	}
 	
+	
+	// *******************************************************************************************************
+	// ****************************************** Auxiliary methods ******************************************
+	// *******************************************************************************************************
+
 	private NotificationEvent createNotificationEvent(NotificationType type, Booking booking, String email) {
 		String subject = type.getSubject();
-		
+
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("H:mm");
-        String dateStr = booking.getStart().format(dateFormatter);       
-        String startTimeStr = booking.getStart().format(timeFormatter);  
-        String endTimeStr = booking.getFinish().format(timeFormatter);    
+		DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("H:mm");
+		String dateStr = booking.getStart().format(dateFormatter);       
+		String startTimeStr = booking.getStart().format(timeFormatter);  
+		String endTimeStr = booking.getFinish().format(timeFormatter);    
 		String message = String.format(type.getMessageText(), classroomName(booking.getIdClassroom()), dateStr, startTimeStr, endTimeStr);
-		
+
 		String log = String.format(type.getLogText(), email);
-		
-	    logger.debug("Notification event created: {}", message);
+
+		logger.debug("Notification event created: {}", message);
 		return new NotificationEvent(subject, message, email, log);
 	}
 
 	private String classroomName(int idClassroom) {
-	    logger.debug("Searching classroom name...");
+		logger.debug("Searching classroom name...");
 		return classroomsStore.stream()
 				.filter(classroom -> classroom.getIdClassroom() == idClassroom)
 				.findAny()
