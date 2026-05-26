c6b4aece262c4351fe033eae607485920a24c07d
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-05-26 12:04:05 +0200

    feat: handle MethodArgumentNotValidException in GlobalExceptionHandler

--- src/main/java/dev/jcasaslopez/booking/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/jcasaslopez/booking/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package dev.jcasaslopez.booking.exception;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +25,18 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<StandardResponse> handleNotFound (RuntimeException ex){
 		StandardResponse response = new StandardResponse (ex.getMessage(), null, HttpStatus.NOT_FOUND);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		StandardResponse response = new StandardResponse(LocalDateTime.now(), "Validation failed for one or more fields", errors.toString(), HttpStatus.BAD_REQUEST);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 	}
 	
 }
