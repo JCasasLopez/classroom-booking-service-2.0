4a82dd7b57cbdc11dc52e22a5fec076e300d4d47
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-05-28 18:11:06 +0200

    feat: add watch alert controller and remove redundant @NotNull in Request DTO

---
+++ src/main/java/dev/jcasaslopez/booking/controller/WatchAlertController.java
@@ -0,0 +1,56 @@
+package dev.jcasaslopez.booking.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.jcasaslopez.booking.dto.WatchAlertResponseDto;
+import dev.jcasaslopez.booking.service.WatchAlertService;
+import dev.jcasaslopez.classroom.shared.utility.StandardResponse;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Validated
+@RestController
+public class WatchAlertController {
+
+private static final Logger logger = LoggerFactory.getLogger(WatchAlertController.class);
+	
+	private final WatchAlertService service;
+	
+	public WatchAlertController(WatchAlertService service) {
+		this.service = service;
+	}
+
+	@PostMapping(value="/watch-alerts")
+	public ResponseEntity<StandardResponse> addWatchAlert(@RequestParam @NotNull @Positive Long idBooking) {
+		logger.debug("POST /watch-alerts - idBooking={}", idBooking);
+		WatchAlertResponseDto watchAlert = service.addWatchAlert(idBooking);
+		
+		StandardResponse response = new StandardResponse("Watch alert created successfully", watchAlert, HttpStatus.CREATED);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	// No need to pass any user information as a parameter, as the endpoint needs the user to be authenticated, 
+	// and the user's email is held in UserContext.
+	@GetMapping(value="/watch-alerts")
+	public ResponseEntity<StandardResponse> getWatchAlertsByUser(@RequestParam @NotNull LocalDateTime startSearch, 
+			@RequestParam @NotNull LocalDateTime finishSearch) {
+		logger.debug("GET /watch-alerts - start={} - finish={}", startSearch, finishSearch);
+		
+		List<WatchAlertResponseDto> watchAlerts = service.watchAlertsListByUserAndTimePeriod(startSearch, finishSearch);
+		
+		StandardResponse response = new StandardResponse("Watch alerts retrieved successfully", watchAlerts, HttpStatus.OK);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+		
+}
--- src/main/java/dev/jcasaslopez/booking/dto/WatchAlertRequestDto.java
+++ src/main/java/dev/jcasaslopez/booking/dto/WatchAlertRequestDto.java
@@ -1,6 +1,3 @@
 package dev.jcasaslopez.booking.dto;
 
-import jakarta.validation.constraints.NotNull;
-
-public record WatchAlertRequestDto (
-		@NotNull(message = "idBooking field is required") Long idBooking) {}
+public record WatchAlertRequestDto (Long idBooking) {}
