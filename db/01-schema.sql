e3508d39644a092e618e5cc757225d9c9afd8861
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-07-21 11:09:02 +0200

    feat(infra): add Docker containerization, database schemas, and Kafka setup

--- db/01-schema.sql
+++ db/01-schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS bookings (
-    id_booking BIGINT NOT NULL AUTO_INCREMENT,
-    id_user INT NOT NULL,
-    id_classroom INT NOT NULL,
+    idBooking BIGINT NOT NULL AUTO_INCREMENT,
+    idUser INT NOT NULL,
+    idClassroom INT NOT NULL,
     start DATETIME NOT NULL,
     finish DATETIME NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     status ENUM('ACTIVE', 'CANCELLED', 'COMPLETED') NOT NULL,
-    PRIMARY KEY (id_booking)
+    PRIMARY KEY (idBooking)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS watch_alerts (
-    id_watch_alert BIGINT NOT NULL AUTO_INCREMENT,
-    id_booking BIGINT NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_watch_alert)
+    idWatchAlert BIGINT NOT NULL AUTO_INCREMENT,
+    idBooking BIGINT NOT NULL,
+    userEmail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (idWatchAlert)
 ) ENGINE=InnoDB;
